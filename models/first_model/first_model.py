08f3d34c6d766dab90cdea8392caa55ecdffc442
mtnntn <a.matinata@gmail.com>
2018-03-20 01:00:06 +0100

    Added environment dl_cpu_py35.yml

--- models/first_model/first_model.py
+++ models/first_model/first_model.py
@@ -1,3 +1,4 @@
+from os import path
 import numpy as np
 from sklearn.model_selection import train_test_split
 
@@ -5,46 +6,49 @@ from keras.layers import Dense
 from keras.models import Sequential
 from keras.optimizers import SGD
 
-# DEFINE CONSTANTS
+# DEFINING CONSTANTS
 random_seed = 7
 
 # # INSTANCE'S CONSTANTS
 sources_num = 8
 
-# # MODEL'S CON STANTS
+# # MODEL'S CONSTANTS
 train_set_size = 0.9
 
 learning_rate = 0.001
 batch_size = 10
-epochs = 50  # train iterations
+epochs = 150  # train iterations
 
-# PRE-PROCESS DATA
+# PRE-PROCESSING DATA
+dataset_path = path.join(path.join("..", ".."), "dataset")
+dataset_file = ""
 
-# I will have a social sensing matrix.
+# We will have a social sensing matrix.
 # Each row represent sources and what this sources claim for each claims
-# Each Column is therefore a claim
 
-# The input of the net is a vector [S1Ci, S2Ci, ..., SmCi]
+# The input of the net is a vector [S1Ci, S2Ci, ..., SmCi] (all the claims f
 # In other word the input are the columns of the SC matrix.
 
 # # LOAD DATASET
-raw_dataset = np.loadtxt("dataset/dataset.csv", delimiter=",")
+raw_dataset = np.loadtxt(path.join(dataset_path, dataset_file), delimiter=",")
 
 # # SPLIT IN TRAIN SET AND DATA SET.
 train_set, test_set = train_test_split(raw_dataset, train_size=train_set_size, random_state=random_seed)
 
-# # EXTRACT LABELS (Yt)
+# # EXTRACT LABELS (Yt) and SAMPLES (X) For TRAIN-SET
 train_labels = train_set[:, 8]
-
-# # ETRACT SAMPLES (X)
 train_samples = train_set[:, 0:8]
 
+# # EXTRACT LABELS (Yt) and SAMPLES (X) For TEST-SET
+test_labels = test_set[:, 8]
+test_samples = test_set[:, 0:8]
 
 # DEFINE THE MODEL
 
-fhl_units = sources_num*2
-shl_units = sources_num*2
-thl_units = sources_num*2
+# questi saranno di meno
+fhl_units = sources_num
+shl_units = sources_num
+thl_units = sources_num
 
 model = Sequential([
     Dense(units=fhl_units, input_dim=sources_num, activation='relu'),  # input layer
@@ -71,10 +75,17 @@ first = test_set[0]
 yt = first[8, ]
 x = first[0:8, ]
 
-print("First sample on test set: " + str(test_set[0]))
+print("\nFirst sample on test set: " + str(test_set[0]))
 print("yt = " + str(yt))
 print("x = " + str(x))
 
 y = model.predict(np.array([x]))
 
 print(y)
+
+print("\n LOSS AND ACCURACY \n")
+
+evaluation = model.evaluate(x=test_samples, y=test_labels, batch_size=batch_size, verbose=2)
+
+print("\n%s: %.2f%% - %s: %.2f%%" %
+      (model.metrics_names[0], evaluation[0]*100, model.metrics_names[1], evaluation[1]*100))
